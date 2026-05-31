5e60dbc2543e66e4fb9244c4d160ed489f06e0bf
xystudio889 <173288240@qq.com>
2026-05-31 04:52:15 +0000

    Deploying to gh-pages from @ xystudiocode/pyClickMouse@a9328a3523bd2e9ebaca38578adb37afc32a322f 🚀

---
+++ assets/en_develop_contributing_SECURITY.md.DxAgR74q.js
@@ -0,0 +1,19 @@
+import{c as s,Q as a,j as t,m as n}from"./chunks/framework.Cr5Oofs_.js";const k=JSON.parse('{"title":"Security Policy","description":"","frontmatter":{},"headers":[],"relativePath":"en/develop/contributing/SECURITY.md","filePath":"en/develop/contributing/SECURITY.md"}'),e={name:"en/develop/contributing/SECURITY.md"};function l(o,i,r,p,h,c){return a(),t("div",null,[...i[0]||(i[0]=[n(`<h1 id="security-policy" tabindex="-1">Security Policy <a class="header-anchor" href="#security-policy" aria-label="Permalink to &quot;Security Policy&quot;">​</a></h1><h2 id="how-to-report-a-vulnerability" tabindex="-1">How to report a vulnerability <a class="header-anchor" href="#how-to-report-a-vulnerability" aria-label="Permalink to &quot;How to report a vulnerability&quot;">​</a></h2><p>Commit a GitHub security to report a vulnerability. Provide detailed information about the affected range, trigger condition, and vulnerability details.</p><h2 id="security-policy-1" tabindex="-1">Security Policy <a class="header-anchor" href="#security-policy-1" aria-label="Permalink to &quot;Security Policy&quot;">​</a></h2><div class="language-markdown vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;"># Security Policy</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">## Supported Versions</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">clickmouse &gt;= </span><span style="--shiki-light:#24292E;--shiki-light-font-weight:bold;--shiki-dark:#E1E4E8;--shiki-dark-font-weight:bold;">**version**</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">## Reporting a Vulnerability</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">Use this section to tell people how to report a vulnerability.</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">Tell them where to go, how often they can expect to get an update on a</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">reported vulnerability, what to expect if the vulnerability is accepted or</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">declined, etc.</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">## How to fix</span></span>
+<span class="line"><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">&gt; [</span><span style="--shiki-light:#032F62;--shiki-light-text-decoration:underline;--shiki-dark:#DBEDFF;--shiki-dark-text-decoration:underline;">!TIP</span><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">]</span></span>
+<span class="line"><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">&gt; This section is optional.</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">If the vulnerability is accepted, provide information on how to fix the vulnerability.</span></span></code></pre></div>`,5)])])}const y=s(e,[["render",l]]);export{k as __pageData,y as default};
