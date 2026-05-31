8c6ba4be543a8cceaecbc168a90b61d18ea2436d
xystudio889 <173288240@qq.com>
2026-05-31 05:29:12 +0000

    Deploying to gh-pages from @ xystudiocode/pyClickMouse@2bb394a199eb096063875a4bb93761bfa0060c7b 🚀

---
+++ assets/zh-CN_develop_contributing_license.md.CaDKOifB.js
@@ -0,0 +1,21 @@
+import{c as s,Q as n,j as e,m as t}from"./chunks/framework.COywXlYp.js";const u=JSON.parse('{"title":"软件协议","description":"介绍软件协议","frontmatter":{"title":"软件协议","description":"介绍软件协议","layout":"doc"},"headers":[],"relativePath":"zh-CN/develop/contributing/license.md","filePath":"zh-CN/develop/contributing/license.md"}'),p={name:"zh-CN/develop/contributing/license.md"};function i(o,a,l,r,c,h){return n(),e("div",null,[...a[0]||(a[0]=[t(`<h1 id="软件协议" tabindex="-1">软件协议 <a class="header-anchor" href="#软件协议" aria-label="Permalink to &quot;软件协议&quot;">​</a></h1><h2 id="版权声明" tabindex="-1">版权声明 <a class="header-anchor" href="#版权声明" aria-label="Permalink to &quot;版权声明&quot;">​</a></h2><p><a target="_blank" href="https://icons8.com/icon/13347/mouse">鼠标</a> 的图标 <a target="_blank" href="https://icons8.com">Icons8</a></p><h2 id="开源协议" tabindex="-1">开源协议 <a class="header-anchor" href="#开源协议" aria-label="Permalink to &quot;开源协议&quot;">​</a></h2><p>本软件基于MIT开源协议，最后我会放出协议内容，你可以复制、修改、分发本软件及文档站，但你必须在源代码中保留原作者信息和协议声明。</p><p>本软件及文档站均开源在github上，你可以在<a href="https://github.com/xystudiocode/pyClickMouse" target="_blank" rel="noreferrer">软件链接</a>打开</p><p>同时软件的源代码开源在gitee上，方便中国用户下载。<a href="https://gitee.com/xystudio889/pyClickMouse" target="_blank" rel="noreferrer">软件链接</a></p><h2 id="商业授权" tabindex="-1">商业授权 <a class="header-anchor" href="#商业授权" aria-label="Permalink to &quot;商业授权&quot;">​</a></h2><p>我们不反对商业授权，你可以在无需联系作者的情况下，将本软件用于商业用途，但必须在源代码中保留原作者信息和协议声明。</p><h2 id="协议内容" tabindex="-1">协议内容 <a class="header-anchor" href="#协议内容" aria-label="Permalink to &quot;协议内容&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>MIT License</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Copyright (c) 2026 xystudiocode</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Permission is hereby granted, free of charge, to any person obtaining a copy</span></span>
+<span class="line"><span>of this software and associated documentation files (the &quot;Software&quot;), to deal</span></span>
+<span class="line"><span>in the Software without restriction, including without limitation the rights</span></span>
+<span class="line"><span>to use, copy, modify, merge, publish, distribute, sublicense, and/or sell</span></span>
+<span class="line"><span>copies of the Software, and to permit persons to whom the Software is</span></span>
+<span class="line"><span>furnished to do so, subject to the following conditions:</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>The above copyright notice and this permission notice shall be included in all</span></span>
+<span class="line"><span>copies or substantial portions of the Software.</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR</span></span>
+<span class="line"><span>IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,</span></span>
+<span class="line"><span>FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE</span></span>
+<span class="line"><span>AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER</span></span>
+<span class="line"><span>LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,</span></span>
+<span class="line"><span>OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE</span></span>
+<span class="line"><span>SOFTWARE.</span></span></code></pre></div>`,11)])])}const T=s(p,[["render",i]]);export{u as __pageData,T as default};
